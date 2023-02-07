958dbf9a2b35ed8a365078e7184fac31ed9ad6eb
Phuc Do <ngocphucdo2601@gmail.com>
2023-02-07 16:08:35 +0700

    First learn basic asp.net web mvc with net5 b01

---
+++ LearnBaProMvcb01/Migrations/20230207074327_InitalDatabase.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LearnBaProMvcb01.Migrations
+{
+    public partial class InitalDatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Departments",
+                columns: table => new
+                {
+                    IdDept = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    NameDept = table.Column<string>(type: "nvarchar(250)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Departments", x => x.IdDept);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    IdEmp = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    NameEmp = table.Column<string>(type: "nvarchar(250)", nullable: false),
+                    Position = table.Column<string>(type: "nvarchar(250)", nullable: false),
+                    PhotoPath = table.Column<string>(type: "nvarchar(500)", nullable: true),
+                    EnrollDate = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    IdDept = table.Column<int>(type: "int", nullable: false),
+                    DepartmentIdDept = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.IdEmp);
+                    table.ForeignKey(
+                        name: "FK_Employees_Departments_DepartmentIdDept",
+                        column: x => x.DepartmentIdDept,
+                        principalTable: "Departments",
+                        principalColumn: "IdDept",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Employees_DepartmentIdDept",
+                table: "Employees",
+                column: "DepartmentIdDept");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Employees");
+
+            migrationBuilder.DropTable(
+                name: "Departments");
+        }
+    }
+}
---
+++ LearnBaProMvcb01/Models/EmployeesController.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+
+namespace LearnBaProMvcb01.Models
+{
+    public class EmployeesController : Controller
+    {
+        private readonly EmployeeDatabaseContext _context;
+
+        public EmployeesController(EmployeeDatabaseContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Employees
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Employees.ToListAsync());
+        }
+
+        // GET: Employees/Details/5
+       /* public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var employee = await _context.Employees
+                .FirstOrDefaultAsync(m => m.IdEmp == id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+
+            return View(employee);
+        }*/
+
+        // GET: Employees/Create
+        public IActionResult AddOrEdit(int id=0)
+        {
+            Employee emp = new Employee();
+            if (id == 0)
+            {
+                return View(emp);
+            } else
+            {
+                emp = _context.Employees.Find(id);
+                return View(emp);
+
+            }
+        }
+
+        // POST: Employees/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> AddOrEdit([Bind("IdEmp,NameEmp,Position,PhotoPath,EnrollDate,IdDept")] Employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                if(employee.IdEmp == 0)
+                {
+                    _context.Add(employee);
+                    await _context.SaveChangesAsync();
+                } else
+                {
+                    _context.Update(employee);
+                    await _context.SaveChangesAsync();
+                }
+                
+                return RedirectToAction(nameof(Index));
+            }
+            return View(employee);
+        }
+
+        // GET: Employees/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var employee = await _context.Employees.FindAsync(id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+            return View(employee);
+        }
+
+        // POST: Employees/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        /*[HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("IdEmp,NameEmp,Position,PhotoPath,EnrollDate,IdDept")] Employee employee)
+        {
+            if (id != employee.IdEmp)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(employee);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!EmployeeExists(employee.IdEmp))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(employee);
+        }*/
+
+        // GET: Employees/Delete/5
+        /*public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var employee = await _context.Employees
+                .FirstOrDefaultAsync(m => m.IdEmp == id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+
+            return View(employee);
+        }*/
+
+        // POST: Employees/Delete/5
+        /*[HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]*/
+        public async Task<IActionResult> DeleteConfirm(int id)
+        {
+            var employee = await _context.Employees.FindAsync(id);
+            _context.Employees.Remove(employee);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool EmployeeExists(int id)
+        {
+            return _context.Employees.Any(e => e.IdEmp == id);
+        }
+    }
+}
