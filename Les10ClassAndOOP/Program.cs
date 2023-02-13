0d2676eaba82a2c1ee946913a041467b36979288
Phuc Do <ngocphucdo2601@gmail.com>
2023-02-13 09:43:08 +0700

    first learn basic class and OOP in c#

---
+++ Les10ClassAndOOP/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace Les10ClassAndOOP
+{
+    class Student : IDisposable
+    {
+        public string name;
+
+        public Student(string name)
+        {
+            this.name = name;
+            Console.WriteLine("Khoi tao "+this.name);
+        }
+
+        ~Student()
+        {
+            Console.ForegroundColor= ConsoleColor.Red;
+            Console.WriteLine("Destructor of Student(Phuong thuc huy cua Student)");
+            Console.ResetColor();
+        }
+
+        public void Dispose()
+        {
+            //phuong thuc giai phong tai nguyen dang vi chiem du, hoac dong chuoi ket noi
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Get memory of Student(Phuong thuc huy cua Student) by Dispose method");
+            Console.ResetColor();
+        }
+    }
+
+    public class Program
+    {
+
+        static void Test()
+        {
+            using Student st = new Student("Ten sinh vien of test mt");
+        }
+        static void Main(string[] args)
+        {
+            /*Weapon wp1; // bien nay vua doi khoi tao; khong cho vo mot gia nao ca ne gia cua no la null
+
+            wp1 = new Weapon();*/
+
+            /* Weapon wp2 = new Weapon("Xin Chao dt 2");
+             wp2.name = "Sung luc";
+             Console.WriteLine(wp2.name);
+             wp2.SetDoSatThuong(5);
+             wp2.TanCong();*/
+
+            /*Weapon vk3 = new Weapon("Sung may", 15);
+            vk3.TanCong();
+            vk3.SatThuong = 600;
+            Console.WriteLine(vk3.SatThuong);
+            vk3.NoiSanXuat = "America";
+            Console.WriteLine(vk3.NoiSanXuat);*/
+
+            /*Student st;
+            for (int i = 0;i < 100000; i++) {
+                st = new Student("Sinh Vien thu "+i);
+                st = null;
+
+            }*/
+
+            //.net core : ep thu hoi bo nho ngay lap tuc bi bo, do dotnet quyet dinh
+            /*GC.Collect();*/
+
+            /*            using (Student st = new Student("Ten Sinh Vien"))
+                        {
+                            // code se thi hanh trong using nay
+                            // ngoai pham vi khoi nay thi se bi thu hoi vung nho
+                        }*/
+
+
+            Test();
+
+        }
+    }
+}
---
+++ Les10ClassAndOOP/Weapon.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Les10ClassAndOOP
+{
+    /**
+     * Access Modify: 
+     * public : co the truy cap tu bat cu dau
+     * internal: chi co the truy cap khi cung mot assembly (cung mot truong trinh)
+     * private: chi truy cap trong class do
+     * protected: truy cap trong class va thu muc cung nhu la class con ke thua, va thu muc con
+     * protected internal: truy cập được khi cùng assembly hoặc lớp kế thừa
+        private protected: truy cập từ lớp chứa nó, lớp kế thừa nhưng phải cùng assembly
+     * Note: Mac dinh trong c#, khi khong chi ra pham vi truy cap cua class la internal
+     */
+
+    /**
+     * mac dinh khi tao class se lun co khoi tao mot constructor rong
+     */
+
+    /**
+     * Destrutor: ham huy (phuong thuc huy) dc chay tu dong khi doi tuong do dc giai phong khoi bo nho
+     */
+    public class Weapon
+    {
+        // mac dinh khi khong chi ra pvtt cua bien la private
+        public string name = "Ten vu khi";
+        public int doSatThuong = 0;
+
+        /**
+         * Thuoc tinh
+         */
+        public int SatThuong {
+            get
+            {
+                return doSatThuong;
+            } set
+            {
+                doSatThuong = value;
+            } }
+
+        public string NoiSanXuat { get; set; }
+
+
+        public Weapon()
+        {
+            /*Console.WriteLine("This is constructor of class Weapon! Lun thi hanh phuong thuc khoi tao dau tien");*/
+        }
+
+        public Weapon(string abc)
+        {
+            Console.WriteLine(abc);
+        }
+
+        public Weapon(string name, int doSatThuong) 
+        {
+            this.name = name;
+            this.doSatThuong = doSatThuong;
+        }
+
+        public void SetDoSatThuong(int doSatThuong)
+        {
+            this.doSatThuong=doSatThuong;
+        }
+
+        public int GetDoSatThuong()
+        {
+            return this.doSatThuong;
+        }
+
+        /**
+         * tu khoa this - ref: tham chieu den doi tuong khai bao
+         */
+
+        public void TanCong()
+        {
+            Console.WriteLine(this.name +":\t");
+            for(int i = 0; i < doSatThuong; i++)
+            {
+                Console.Write("    *   ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
