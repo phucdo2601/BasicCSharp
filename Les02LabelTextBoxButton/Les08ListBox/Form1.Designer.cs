29ace94f3ab524e3564cd045eeed005a7069c7c9
Phuc Do Ngoc <ngocphucdo2601@gmail.com>
2023-11-09 17:07:14 +0700

    [winform] les08-List box b01

---
+++ Les02LabelTextBoxButton/Les08ListBox/Form1.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace Les08ListBox
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstDanSach = new System.Windows.Forms.ListBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lstDanSach
+            // 
+            this.lstDanSach.FormattingEnabled = true;
+            this.lstDanSach.ItemHeight = 16;
+            this.lstDanSach.Items.AddRange(new object[] {
+            "item_1",
+            "item_2",
+            "item_3",
+            "item_4"});
+            this.lstDanSach.Location = new System.Drawing.Point(177, 121);
+            this.lstDanSach.Name = "lstDanSach";
+            this.lstDanSach.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
+            this.lstDanSach.Size = new System.Drawing.Size(197, 148);
+            this.lstDanSach.TabIndex = 0;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(435, 121);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 1;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.lstDanSach);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstDanSach;
+        private System.Windows.Forms.Button btnAdd;
+    }
+}
+
---
+++ Les02LabelTextBoxButton/Les08ListBox/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Les08ListBox
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            lstDanSach.Items.Add("item_5");
+            lstDanSach.Items.Add("item_6");
+            lstDanSach.Items.Add("item_7");
+        }
+    }
+}
