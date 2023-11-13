8eb1d4fe19da9091733630e00ee76e2625b486c7
Phuc Do Ngoc <ngocphucdo2601@gmail.com>
2023-11-13 16:40:00 +0700

    [WIN-FORM] 17. Basic learn list view b01

---
+++ Les02LabelTextBoxButton/Les17ListView/Form1.Designer.cs
@@ -0,0 +1,227 @@
+﻿namespace Les17ListView
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.ListViewItem listViewItem7 = new System.Windows.Forms.ListViewItem(new string[] {
+            "01",
+            "test-ten-sp-01",
+            "50000"}, -1);
+            System.Windows.Forms.ListViewItem listViewItem8 = new System.Windows.Forms.ListViewItem(new string[] {
+            "02",
+            "test-ten-sp-02",
+            "45000"}, -1);
+            System.Windows.Forms.ListViewItem listViewItem9 = new System.Windows.Forms.ListViewItem(new string[] {
+            "03",
+            "test-ten-sp-03",
+            "234234"}, -1);
+            this.lvSanPham = new System.Windows.Forms.ListView();
+            this.clnMaSp = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.clnTenSp = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.clnGiaSp = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtItemId = new System.Windows.Forms.TextBox();
+            this.txtItemPrice = new System.Windows.Forms.TextBox();
+            this.txtItemName = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnRemoveIndex = new System.Windows.Forms.Button();
+            this.btnRemoveItem = new System.Windows.Forms.Button();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lvSanPham
+            // 
+            this.lvSanPham.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.clnMaSp,
+            this.clnTenSp,
+            this.clnGiaSp});
+            this.lvSanPham.FullRowSelect = true;
+            this.lvSanPham.GridLines = true;
+            this.lvSanPham.HideSelection = false;
+            this.lvSanPham.Items.AddRange(new System.Windows.Forms.ListViewItem[] {
+            listViewItem7,
+            listViewItem8,
+            listViewItem9});
+            this.lvSanPham.Location = new System.Drawing.Point(86, 204);
+            this.lvSanPham.Name = "lvSanPham";
+            this.lvSanPham.Size = new System.Drawing.Size(393, 214);
+            this.lvSanPham.TabIndex = 0;
+            this.lvSanPham.UseCompatibleStateImageBehavior = false;
+            this.lvSanPham.View = System.Windows.Forms.View.Details;
+            this.lvSanPham.ColumnClick += new System.Windows.Forms.ColumnClickEventHandler(this.lvSanPham_ColumnClick);
+            this.lvSanPham.SelectedIndexChanged += new System.EventHandler(this.lvSanPham_SelectedIndexChanged);
+            // 
+            // clnMaSp
+            // 
+            this.clnMaSp.Text = "Ma San Pham";
+            this.clnMaSp.Width = 120;
+            // 
+            // clnTenSp
+            // 
+            this.clnTenSp.Text = "Ten SP";
+            this.clnTenSp.Width = 120;
+            // 
+            // clnGiaSp
+            // 
+            this.clnGiaSp.Text = "Gia SP";
+            this.clnGiaSp.Width = 120;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "MaSp";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(26, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "TenSp";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(26, 132);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(28, 16);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Gia";
+            // 
+            // txtItemId
+            // 
+            this.txtItemId.Location = new System.Drawing.Point(157, 6);
+            this.txtItemId.Name = "txtItemId";
+            this.txtItemId.Size = new System.Drawing.Size(172, 22);
+            this.txtItemId.TabIndex = 4;
+            // 
+            // txtItemPrice
+            // 
+            this.txtItemPrice.Location = new System.Drawing.Point(157, 132);
+            this.txtItemPrice.Name = "txtItemPrice";
+            this.txtItemPrice.Size = new System.Drawing.Size(172, 22);
+            this.txtItemPrice.TabIndex = 5;
+            // 
+            // txtItemName
+            // 
+            this.txtItemName.Location = new System.Drawing.Point(157, 69);
+            this.txtItemName.Name = "txtItemName";
+            this.txtItemName.Size = new System.Drawing.Size(172, 22);
+            this.txtItemName.TabIndex = 6;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(371, 5);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 7;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnRemoveIndex
+            // 
+            this.btnRemoveIndex.Location = new System.Drawing.Point(371, 66);
+            this.btnRemoveIndex.Name = "btnRemoveIndex";
+            this.btnRemoveIndex.Size = new System.Drawing.Size(120, 23);
+            this.btnRemoveIndex.TabIndex = 8;
+            this.btnRemoveIndex.Text = "Remove Index";
+            this.btnRemoveIndex.UseVisualStyleBackColor = true;
+            this.btnRemoveIndex.Click += new System.EventHandler(this.btnRemove_Click);
+            // 
+            // btnRemoveItem
+            // 
+            this.btnRemoveItem.Location = new System.Drawing.Point(371, 132);
+            this.btnRemoveItem.Name = "btnRemoveItem";
+            this.btnRemoveItem.Size = new System.Drawing.Size(75, 23);
+            this.btnRemoveItem.TabIndex = 9;
+            this.btnRemoveItem.Text = "Remove";
+            this.btnRemoveItem.UseVisualStyleBackColor = true;
+            this.btnRemoveItem.Click += new System.EventHandler(this.btnRemoveItem_Click);
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.Location = new System.Drawing.Point(511, 5);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(75, 23);
+            this.btnEdit.TabIndex = 10;
+            this.btnEdit.Text = "Edit";
+            this.btnEdit.UseVisualStyleBackColor = true;
+            this.btnEdit.Click += new System.EventHandler(this.btnEdit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnEdit);
+            this.Controls.Add(this.btnRemoveItem);
+            this.Controls.Add(this.btnRemoveIndex);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtItemName);
+            this.Controls.Add(this.txtItemPrice);
+            this.Controls.Add(this.txtItemId);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lvSanPham);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView lvSanPham;
+        private System.Windows.Forms.ColumnHeader clnMaSp;
+        private System.Windows.Forms.ColumnHeader clnTenSp;
+        private System.Windows.Forms.ColumnHeader clnGiaSp;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtItemId;
+        private System.Windows.Forms.TextBox txtItemPrice;
+        private System.Windows.Forms.TextBox txtItemName;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnRemoveIndex;
+        private System.Windows.Forms.Button btnRemoveItem;
+        private System.Windows.Forms.Button btnEdit;
+    }
+}
+
---
+++ Les02LabelTextBoxButton/Les17ListView/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Les17ListView
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lvSanPham_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (lvSanPham.SelectedItems.Count > 0)
+            {
+                ListViewItem lv1 = lvSanPham.SelectedItems[0];
+                string ma = lv1.SubItems[0].Text;
+                string name = lv1.SubItems[1].Text;
+                string price = lv1.SubItems[2].Text;
+                //MessageBox.Show(ma+ "/" +name+ "/"+price);
+
+                txtItemId.Text = ma;
+                txtItemName.Text = name;
+                txtItemPrice.Text = price;
+            }
+        }
+
+        private void lvSanPham_ColumnClick(object sender, ColumnClickEventArgs e)
+        {
+            if (e.Column != -1)
+            {
+                ColumnHeader col = lvSanPham.Columns[e.Column];
+                MessageBox.Show("Cot ban chon: "+col.Text);
+            }
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            ListViewItem lv1 = new ListViewItem(txtItemId.Text);
+            //them cac o tiep theo
+            lv1.SubItems.Add(txtItemName.Text);
+            lv1.SubItems.Add(txtItemPrice.Text);
+
+            lvSanPham.Items.Add(lv1);
+        }
+
+        private void btnRemove_Click(object sender, EventArgs e)
+        {
+            while (lvSanPham.SelectedItems.Count > 0)
+            {
+                Console.WriteLine(lvSanPham.SelectedItems[0].Index);
+                lvSanPham.Items.RemoveAt(lvSanPham.SelectedItems[0].Index);
+            }
+        }
+
+        private void btnRemoveItem_Click(object sender, EventArgs e)
+        {
+            while (lvSanPham.SelectedItems.Count > 0)
+            {
+                Console.WriteLine(lvSanPham.SelectedItems[0].Index);
+                lvSanPham.Items.Remove(lvSanPham.SelectedItems[0]);
+                MessageBox.Show("da xoa 1 dong");
+            }
+        }
+
+        private void btnEdit_Click(object sender, EventArgs e)
+        {
+            if (lvSanPham.SelectedItems.Count > 0)
+            {
+                ListViewItem lv1 = lvSanPham.SelectedItems[0];
+                lv1.SubItems[0].Text = txtItemId.Text;
+                lv1.SubItems[1].Text = txtItemName.Text;
+                lv1.SubItems[2].Text = txtItemPrice.Text;
+            }
+        }
+    }
+}
