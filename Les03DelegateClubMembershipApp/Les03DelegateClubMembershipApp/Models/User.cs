00bbd4de858396e2e7d3518214a5841095f7e7c3
Phuc Do Ngoc <ngocphucdo2601@gmail.com>
2023-11-14 15:02:38 +0700

    [csharp-advance] 02. Learn delegate with pro b01

---
+++ Les03DelegateClubMembershipApp/Les03DelegateClubMembershipApp/Models/User.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Les03DelegateClubMembershipApp.Models
+{
+    
+    public class User
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public string Id { get; set; }
+        public string FirstName { get; set; }
+        private string LastName { get; set; }
+        public string  Password { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public string AddressFirstLine { get; set; }
+        public string AddressSecondLine { get; set; }
+        public string AddressCity { get; set; }
+
+        public string PostCode { get; set; }
+    }
+}
---
+++ Les03DelegateClubMembershipApp/Les03DelegateClubMembershipApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Les03DelegateClubMembershipApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
