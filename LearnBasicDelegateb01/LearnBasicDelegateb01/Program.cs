799cfa2b9e8d3c3e766bd55d2705709edb75b9b5
Phuc Do Ngoc <ngocphucdo2601@gmail.com>
2023-11-14 14:18:33 +0700

    [csharp-advance] 01. Basic learn delegate b01

---
+++ LearnBasicDelegateb01/LearnBasicDelegateb01/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+
+namespace LearnBasicDelegateb01
+{
+    public class Program
+    {
+        delegate void LogDel(string text);
+
+        static void Main(string[] args)
+        {
+            //LogDel logDel = new LogDel(LogTextToScreen);
+            //LogDel logDel = new LogDel(LogTextToFile);
+
+            Log log = new Log();
+
+            LogDel LogTextToScreenDel, LogTextToFileDel;
+
+            LogTextToScreenDel = new LogDel(log.LogTextToScreen);
+            LogTextToFileDel = new LogDel(log.LogTextToFile);
+
+            LogDel multiLogDel = LogTextToScreenDel + LogTextToFileDel;
+
+            Console.WriteLine("Please input your name: ");
+            var inputVar = Console.ReadLine();
+            //multiLogDel(inputVar);
+
+            LogText(LogTextToScreenDel, inputVar);
+
+
+            Console.ReadKey();
+        }
+
+        static void LogText(LogDel logDel, string text)
+        {
+            logDel(text);
+        }
+    }
+
+    public class Log
+    {
+        public void LogTextToScreen(string text)
+        {
+            Console.WriteLine($"{DateTime.Now}: {text}");
+        }
+
+        public void LogTextToFile(string text)
+        {
+            using (StreamWriter sw = new StreamWriter(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Log.txt"), true))
+            {
+                sw.WriteLine($"{DateTime.Now}: {text}");
+            }
+        }
+    }
+}
