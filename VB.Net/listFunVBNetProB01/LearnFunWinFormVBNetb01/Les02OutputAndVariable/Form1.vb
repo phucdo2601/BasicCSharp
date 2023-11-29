de300e0529363b19597fe5f28822803e207dac3a
Phuc Do Ngoc <ngocphucdo2601@gmail.com>
2023-11-29 15:16:50 +0700

    [learn-basic-vb.net] 02. Basic learn variable in vb

---
+++ VB.Net/listFunVBNetProB01/LearnFunWinFormVBNetb01/Les02OutputAndVariable/Form1.vb
@@ -0,0 +1,28 @@
+﻿Public Class Form1
+    Dim testVar As String = "Hello World. Welcome to my channel!"
+
+
+    Private Sub btnGo_Click(sender As Object, e As EventArgs) Handles btnGo.Click
+
+        MsgBox(testVar)
+
+        MessageBox.Show("This is an another message!")
+
+
+
+    End Sub
+
+    Private Sub btnVariable_Click(sender As Object, e As EventArgs) Handles btnVariable.Click
+
+        Dim sFirstName As String
+        Dim sLastName As String
+        sFirstName = "test-first-name-b01"
+        sLastName = "t-lastname-b02"
+        MsgBox("Hello world and welcome there, " & sFirstName & " " & sLastName & ". I am hero")
+
+        sFirstName = "test-first-name-b01-up"
+        sLastName = "t-lastname-b02-up"
+        MsgBox("2nd Mess Hello world and welcome there, " & sFirstName & " " & sLastName & ". I am hero")
+
+    End Sub
+End Class
