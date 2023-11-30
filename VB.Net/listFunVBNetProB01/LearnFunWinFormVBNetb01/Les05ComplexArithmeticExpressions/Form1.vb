fe4f0c5a8485a50809e6f22ea09de4ffa23a9a7b
Phuc Do Ngoc <ngocphucdo2601@gmail.com>
2023-11-30 14:32:18 +0700

    [learn-basic-vb.net] 07. First learn basic in VB.Net complex Algorithm logic b01

---
+++ VB.Net/listFunVBNetProB01/LearnFunWinFormVBNetb01/Les05ComplexArithmeticExpressions/Form1.vb
@@ -0,0 +1,33 @@
+﻿Public Class Form1
+    Private Sub btnCalculate_Click(sender As Object, e As EventArgs) Handles btnCalculate.Click
+        Dim decPrice As Decimal
+        Dim iQuantity As Integer
+        Dim decDiscount As Decimal
+        Dim decTotalCost As Decimal
+        Dim decPostage As Decimal
+
+        decPrice = 7
+        iQuantity = 10
+        decDiscount = 3
+        decPostage = 2
+
+        decTotalCost = (decPrice - decDiscount) * iQuantity + decPostage
+        MsgBox(decTotalCost)
+
+
+        'BO(DM)(AS) - Bracket Order Divsion Multiplication Addition Substraction
+        'Please execute my dear aunt PE(MD)AS - Parentheses Exponentitation Multiplication Division Addition Substraction
+
+        Dim iResult As Integer
+        iResult = 10 - 5 + 2
+        MsgBox(iResult)
+
+        iResult = 10 + 5 - 2
+        MsgBox(iResult)
+
+        iResult = 10 - 5 + 2
+        MsgBox(iResult)
+
+
+    End Sub
+End Class
