ee1d600d658abbac81f15b4437aedf8c6166dc06
Phuc Do Ngoc <ngocphucdo2601@gmail.com>
2023-11-30 10:20:51 +0700

    [learn-basic-vb.net] 05. First learn basic TextInput in VB.Net b01

--- VB.Net/listFunVBNetProB01/LearnFunWinFormVBNetb01/Les03LearnInputInVbNet/Form1.Designer.vb
+++ VB.Net/listFunVBNetProB01/LearnFunWinFormVBNetb01/Les03LearnInputInVbNet/Form1.Designer.vb
@@ -23,31 +23,105 @@ Partial Class Form1
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.btnGo = New System.Windows.Forms.Button()
-        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.txtFirstName = New System.Windows.Forms.TextBox()
+        Me.lblFirstName = New System.Windows.Forms.Label()
+        Me.txtLastName = New System.Windows.Forms.TextBox()
+        Me.lblLastName = New System.Windows.Forms.Label()
+        Me.txtGender = New System.Windows.Forms.TextBox()
+        Me.lblGender = New System.Windows.Forms.Label()
+        Me.lstOccupation = New System.Windows.Forms.ListBox()
+        Me.lblDescrition = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
         'btnGo
         '
-        Me.btnGo.Location = New System.Drawing.Point(149, 188)
+        Me.btnGo.Location = New System.Drawing.Point(217, 252)
         Me.btnGo.Name = "btnGo"
         Me.btnGo.Size = New System.Drawing.Size(94, 71)
         Me.btnGo.TabIndex = 0
         Me.btnGo.Text = "Go"
         Me.btnGo.UseVisualStyleBackColor = True
         '
-        'TextBox1
+        'txtFirstName
         '
-        Me.TextBox1.Location = New System.Drawing.Point(143, 13)
-        Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(248, 22)
-        Me.TextBox1.TabIndex = 1
+        Me.txtFirstName.Location = New System.Drawing.Point(198, 59)
+        Me.txtFirstName.Name = "txtFirstName"
+        Me.txtFirstName.Size = New System.Drawing.Size(248, 22)
+        Me.txtFirstName.TabIndex = 1
+        '
+        'lblFirstName
+        '
+        Me.lblFirstName.AutoSize = True
+        Me.lblFirstName.Location = New System.Drawing.Point(68, 59)
+        Me.lblFirstName.Name = "lblFirstName"
+        Me.lblFirstName.Size = New System.Drawing.Size(72, 16)
+        Me.lblFirstName.TabIndex = 2
+        Me.lblFirstName.Text = "First Name"
+        '
+        'txtLastName
+        '
+        Me.txtLastName.Location = New System.Drawing.Point(198, 136)
+        Me.txtLastName.Name = "txtLastName"
+        Me.txtLastName.Size = New System.Drawing.Size(248, 22)
+        Me.txtLastName.TabIndex = 3
+        '
+        'lblLastName
+        '
+        Me.lblLastName.AutoSize = True
+        Me.lblLastName.Location = New System.Drawing.Point(67, 136)
+        Me.lblLastName.Name = "lblLastName"
+        Me.lblLastName.Size = New System.Drawing.Size(72, 16)
+        Me.lblLastName.TabIndex = 4
+        Me.lblLastName.Text = "Last Name"
+        '
+        'txtGender
+        '
+        Me.txtGender.Location = New System.Drawing.Point(198, 191)
+        Me.txtGender.Name = "txtGender"
+        Me.txtGender.Size = New System.Drawing.Size(248, 22)
+        Me.txtGender.TabIndex = 5
+        '
+        'lblGender
+        '
+        Me.lblGender.AutoSize = True
+        Me.lblGender.Location = New System.Drawing.Point(67, 191)
+        Me.lblGender.Name = "lblGender"
+        Me.lblGender.Size = New System.Drawing.Size(52, 16)
+        Me.lblGender.TabIndex = 6
+        Me.lblGender.Text = "Gender"
+        '
+        'lstOccupation
+        '
+        Me.lstOccupation.FormattingEnabled = True
+        Me.lstOccupation.ItemHeight = 16
+        Me.lstOccupation.Items.AddRange(New Object() {"Doctor", "Police", "Engineer", "test-staff-b01", "test-staff-b02", "tutor", "Teacher"})
+        Me.lstOccupation.Location = New System.Drawing.Point(479, 59)
+        Me.lstOccupation.Name = "lstOccupation"
+        Me.lstOccupation.Size = New System.Drawing.Size(186, 228)
+        Me.lstOccupation.TabIndex = 7
+        '
+        'lblDescrition
+        '
+        Me.lblDescrition.AutoSize = True
+        Me.lblDescrition.Location = New System.Drawing.Point(485, 25)
+        Me.lblDescrition.Name = "lblDescrition"
+        Me.lblDescrition.Size = New System.Drawing.Size(75, 16)
+        Me.lblDescrition.TabIndex = 8
+        Me.lblDescrition.Text = "Description"
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.lblDescrition)
+        Me.Controls.Add(Me.lstOccupation)
+        Me.Controls.Add(Me.lblGender)
+        Me.Controls.Add(Me.txtGender)
+        Me.Controls.Add(Me.lblLastName)
+        Me.Controls.Add(Me.txtLastName)
+        Me.Controls.Add(Me.lblFirstName)
+        Me.Controls.Add(Me.txtFirstName)
         Me.Controls.Add(Me.btnGo)
         Me.Name = "Form1"
         Me.Text = "Form1"
@@ -57,5 +131,12 @@ Partial Class Form1
     End Sub
 
     Friend WithEvents btnGo As Button
-    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents txtFirstName As TextBox
+    Friend WithEvents lblFirstName As Label
+    Friend WithEvents txtLastName As TextBox
+    Friend WithEvents lblLastName As Label
+    Friend WithEvents txtGender As TextBox
+    Friend WithEvents lblGender As Label
+    Friend WithEvents lstOccupation As ListBox
+    Friend WithEvents lblDescrition As Label
 End Class
--- VB.Net/listFunVBNetProB01/LearnFunWinFormVBNetb01/Les03LearnInputInVbNet/Form1.vb
+++ VB.Net/listFunVBNetProB01/LearnFunWinFormVBNetb01/Les03LearnInputInVbNet/Form1.vb
@@ -1,7 +1,25 @@
 ﻿Public Class Form1
     Private Sub btnGo_Click(sender As Object, e As EventArgs) Handles btnGo.Click
-        Dim sFirstName As String
-        sFirstName = InputBox("Please enter your first name")
-        MsgBox("Hello " & sFirstName)
+        Dim sFirstname As String
+        Dim sLastname As String
+        Dim sGender As String
+        Dim sOccupation As String
+
+        sFirstname = txtFirstName.Text.Trim
+        sLastname = txtLastName.Text.Trim
+        sGender = txtGender.Text.Trim
+        sOccupation = lstOccupation.SelectedItem
+
+
+
+        MsgBox("Hello " & sFirstname & " " & sLastname & " you are a " & sGender & " " & sOccupation)
+
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        lstOccupation.Items.Add("Writer")
+        lstOccupation.Items.Add("Singer")
+        lstOccupation.Items.Add("Actor")
     End Sub
 End Class
