641fdb2a1ad965bfbcde4b5ad917aa6a2fb90224
Phuc Do Ngoc <ngocphucdo2601@gmail.com>
2023-11-30 17:02:17 +0700

    [learn-basic-vb.net] 08. Basic learn logical inVB.net b01

---
+++ VB.Net/listFunVBNetProB01/LearnFunWinFormVBNetb01/Les07LogicalAndRelational/Form1.Designer.vb
@@ -0,0 +1,73 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lblCode = New System.Windows.Forms.Label()
+        Me.txtExamCode = New System.Windows.Forms.TextBox()
+        Me.btnGetGrade = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'lblCode
+        '
+        Me.lblCode.AutoSize = True
+        Me.lblCode.Location = New System.Drawing.Point(67, 44)
+        Me.lblCode.Name = "lblCode"
+        Me.lblCode.Size = New System.Drawing.Size(171, 16)
+        Me.lblCode.TabIndex = 0
+        Me.lblCode.Text = "Enter the examination code:"
+        '
+        'txtExamCode
+        '
+        Me.txtExamCode.Location = New System.Drawing.Point(259, 44)
+        Me.txtExamCode.Name = "txtExamCode"
+        Me.txtExamCode.Size = New System.Drawing.Size(232, 22)
+        Me.txtExamCode.TabIndex = 1
+        '
+        'btnGetGrade
+        '
+        Me.btnGetGrade.Location = New System.Drawing.Point(146, 116)
+        Me.btnGetGrade.Name = "btnGetGrade"
+        Me.btnGetGrade.Size = New System.Drawing.Size(167, 52)
+        Me.btnGetGrade.TabIndex = 2
+        Me.btnGetGrade.Text = "Get Grade"
+        Me.btnGetGrade.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.btnGetGrade)
+        Me.Controls.Add(Me.txtExamCode)
+        Me.Controls.Add(Me.lblCode)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lblCode As Label
+    Friend WithEvents txtExamCode As TextBox
+    Friend WithEvents btnGetGrade As Button
+End Class
---
+++ VB.Net/listFunVBNetProB01/LearnFunWinFormVBNetb01/Les07LogicalAndRelational/Form1.vb
@@ -0,0 +1,22 @@
+﻿Public Class Form1
+    Private Sub btnGetGrade_Click(sender As Object, e As EventArgs) Handles btnGetGrade.Click
+        Dim iScore As Integer
+        iScore = txtExamCode.Text
+
+        If iScore < 0 Or iScore > 100 Then
+            MsgBox("That is not valid score. Enter a number between 0 and 100")
+            'exit when the program excute this signtag, the program will be exit 
+            Exit Sub
+        End If
+
+        If iScore >= 50 Then
+            MsgBox("pass")
+
+        Else
+            MsgBox("Not Pass")
+        End If
+
+
+        MsgBox("all done")
+    End Sub
+End Class
