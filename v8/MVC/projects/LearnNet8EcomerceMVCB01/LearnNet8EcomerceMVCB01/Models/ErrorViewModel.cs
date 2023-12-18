eb188b4f5db3294183d8b2eb2bf76c454958103b
Phuc Do Ngoc <ngocphucdo2601@gmail.com>
2023-12-18 17:07:18 +0700

    [l-net8-ecweb-mvc-b01] 01. Basic create and init docker mssql server b01

---
+++ v8/MVC/projects/LearnNet8EcomerceMVCB01/LearnNet8EcomerceMVCB01/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LearnNet8EcomerceMVCB01.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
