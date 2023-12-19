b91bc8991d87163fc7bfcd56e67afe3b8c172441
Phuc Do Ngoc <ngocphucdo2601@gmail.com>
2023-12-19 15:04:02 +0700

    [l-net8-ecweb-mvc-b01] 04. Lean basic razor page, razor layout, and view component for designing product page, and LoaiViewCompent b01

---
+++ v8/MVC/projects/LearnNet8ShoppingWebMVCB01/LearnNet8ShoppingWebMVCB01/Controllers/HangHoaController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LearnNet8ShoppingWebMVCB01.Controllers
+{
+	public class HangHoaController : Controller
+	{
+		public IActionResult Index(int? loai)
+		{
+
+			return View();
+		}
+	}
+}
---
+++ v8/MVC/projects/LearnNet8ShoppingWebMVCB01/LearnNet8ShoppingWebMVCB01/ViewModels/MenuLoaiVM.cs
@@ -0,0 +1,9 @@
+﻿namespace LearnNet8ShoppingWebMVCB01.ViewModels
+{
+	public class MenuLoaiVM
+	{
+        public int MaLoai { get; set; }
+        public string TenLoai { get; set; }
+        public int SoLuong { get; set; }
+    }
+}
