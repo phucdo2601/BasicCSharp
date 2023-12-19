423883674b0e6c89b7317971f617a0ffa57ac0c7
Phuc Do Ngoc <ngocphucdo2601@gmail.com>
2023-12-19 17:17:37 +0700

    [l-net8-ecweb-mvc-b01] 05. Basic design search and list product b01

--- v8/MVC/projects/LearnNet8ShoppingWebMVCB01/LearnNet8ShoppingWebMVCB01/Controllers/HangHoaController.cs
+++ v8/MVC/projects/LearnNet8ShoppingWebMVCB01/LearnNet8ShoppingWebMVCB01/Controllers/HangHoaController.cs
@@ -1,13 +1,57 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using LearnNet8ShoppingWebMVCB01.Data;
+using LearnNet8ShoppingWebMVCB01.ViewModels;
+using Microsoft.AspNetCore.Mvc;
 
 namespace LearnNet8ShoppingWebMVCB01.Controllers
 {
 	public class HangHoaController : Controller
 	{
-		public IActionResult Index(int? loai)
-		{
+		private readonly Hshop2023Context db;
 
-			return View();
+        public HangHoaController(Hshop2023Context context)
+        {
+            db = context;
+        }
+        public IActionResult Index(int? loai)
+		{
+			var hangHoas = db.HangHoas.AsQueryable();
+
+            if (loai.HasValue)
+            {
+                hangHoas = hangHoas.Where(p => p.MaLoai == loai.Value);
+            }
+
+            var result = hangHoas.Select(p => new HangHoaVM
+            {
+                MaHH = p.MaHh,
+                TenHH = p.TenHh,
+                DonGia = p.DonGia ?? 0,
+                Hinh = p.Hinh ?? "",
+                MoTaNgan = p.MoTaDonVi ?? "",
+                TenLoai = p.MaLoaiNavigation.TenLoai
+            });
+            return View(result);
 		}
+
+        public IActionResult Search(string? query)
+        {
+            var hangHoas = db.HangHoas.AsQueryable();
+
+            if (query != null)
+            {
+                hangHoas = hangHoas.Where(p => p.TenHh.Contains(query));
+            }
+
+            var result = hangHoas.Select(p => new HangHoaVM
+            {
+                MaHH = p.MaHh,
+                TenHH = p.TenHh,
+                DonGia = p.DonGia ?? 0,
+                Hinh = p.Hinh ?? "",
+                MoTaNgan = p.MoTaDonVi ?? "",
+                TenLoai = p.MaLoaiNavigation.TenLoai
+            });
+            return View(result);
+        }
 	}
 }
---
+++ v8/MVC/projects/LearnNet8ShoppingWebMVCB01/LearnNet8ShoppingWebMVCB01/ViewModels/HangHoaVM.cs
@@ -0,0 +1,12 @@
+﻿namespace LearnNet8ShoppingWebMVCB01.ViewModels
+{
+    public class HangHoaVM
+    {
+        public int MaHH { get; set; }
+        public string TenHH { get; set; }
+        public string Hinh { get; set; }
+        public double DonGia { get; set; }
+        public string MoTaNgan { get; set; }
+        public string TenLoai { get; set; }
+    }
+}
