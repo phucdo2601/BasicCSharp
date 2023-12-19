ae4181353e6200b5a5b2c4f3d0fae4629d3d1501
Phuc Do Ngoc <ngocphucdo2601@gmail.com>
2023-12-19 10:49:11 +0700

    [l-net8-ecweb-mvc-b01] 02. Basic create scaffoling row of data on project b01

---
+++ v8/MVC/projects/LearnNet8ShoppingWebMVCB01/LearnNet8ShoppingWebMVCB01/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LearnNet8ShoppingWebMVCB01.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
