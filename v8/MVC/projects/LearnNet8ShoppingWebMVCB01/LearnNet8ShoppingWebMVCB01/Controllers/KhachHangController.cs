3cf452d37bf5725f24acc222785263dcaa5fee58
Phuc Do Ngoc <ngocphucdo2601@gmail.com>
2023-12-20 17:23:20 +0700

    [l-net8-ecweb-mvc-b01] 09. Update basic view of login page b01

---
+++ v8/MVC/projects/LearnNet8ShoppingWebMVCB01/LearnNet8ShoppingWebMVCB01/Controllers/KhachHangController.cs
@@ -0,0 +1,23 @@
+﻿using LearnNet8ShoppingWebMVCB01.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LearnNet8ShoppingWebMVCB01.Controllers
+{
+    public class KhachHangController : Controller
+    {
+        private readonly Hshop2023Context db;
+
+        public KhachHangController(Hshop2023Context context)
+        {
+            db = context;
+        }
+
+        [HttpGet]
+        public IActionResult DangKy()
+        {
+            return View();
+        }
+
+
+    }
+}
---
+++ v8/MVC/projects/LearnNet8ShoppingWebMVCB01/LearnNet8ShoppingWebMVCB01/ViewModels/RegisterVM.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LearnNet8ShoppingWebMVCB01.ViewModels
+{
+    public class RegisterVM
+    {
+        [Display(Name = "Tên Đăng Nhập")]
+        [Required(ErrorMessage ="*")]
+        [MaxLength(20, ErrorMessage = "Tối đa 20 kí tự")]
+        public string MaKh { get; set; }
+
+        [Display(Name = "Mật khẩu")]
+        [Required(ErrorMessage ="*")]
+        public string? MatKhau { get; set; }
+
+        [Display(Name = "Họ Tên")]
+        [MaxLength(50, ErrorMessage = "Tối đa 50 kí tự")]
+        public string HoTen { get; set; }
+
+        public bool GioiTinh { get; set; } = true;
+
+        public DateTime? NgaySinh { get; set; }
+
+        [MaxLength(60, ErrorMessage = "Tối đa 60 kí tự")]
+        public string DiaChi { get; set; }
+
+        [MaxLength(24, ErrorMessage = "Tối đa 24 kí tự")]
+        [RegularExpression(@"0[9875]\d{8}", ErrorMessage = "Chưa đúng định dạng di động việt nam")]
+        public string DienThoai { get; set; }
+
+        [EmailAddress(ErrorMessage = "Chưa đúng định dạng email")]
+        public string Email { get; set; }
+
+        public string? Hinh { get; set; }
+    }
+}
