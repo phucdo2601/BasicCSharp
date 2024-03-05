706f8b216c0534b1a04a8fd663be951bec727d54
Phuc Do Ngoc <ngocphucdo2601@gmail.com>
2024-03-05 17:51:41 +0700

    [fun-vb] 03. Basic learn data type in VB.net

---
+++ VB.Net/Fundamental/Les01InitVB/Les03DataType/DataType.vb
@@ -0,0 +1,74 @@
+﻿Public Class DataType
+    'Public Property b As Byte
+    '    Get
+    '        Return b
+    '    End Get
+
+    '    Set(value As Byte)
+    '        b = value
+    '    End Set
+    'End Property
+
+    Public Property n As Integer
+        Get
+            Return n
+        End Get
+        Set(value As Integer)
+            n = value
+        End Set
+    End Property
+
+    Public Property si As Single
+        Get
+            Return si
+        End Get
+        Set(value As Single)
+            si = value
+        End Set
+    End Property
+
+    Public Property d As Double
+        Get
+            Return d
+        End Get
+        Set(value As Double)
+            d = value
+        End Set
+    End Property
+
+    Public Property da As Date
+        Get
+            Return da
+        End Get
+        Set(value As Date)
+            da = value
+        End Set
+    End Property
+
+    Public Property c As Char
+        Get
+            Return c
+        End Get
+        Set(value As Char)
+            c = value
+        End Set
+    End Property
+
+    Public Property s As String
+        Get
+            Return s
+        End Get
+        Set(value As String)
+            s = value
+        End Set
+    End Property
+
+    Public Property bl As Boolean
+        Get
+            Return bl
+        End Get
+        Set(value As Boolean)
+            bl = value
+        End Set
+    End Property
+End Class
