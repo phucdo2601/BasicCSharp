1f0baa5c87a9327942fb5944b82f85ceeb432810
Phuc Do Ngoc <ngocphucdo2601@gmail.com>
2025-03-29 14:49:16 +0700

    [learn-net9-mcv-b01] 01. Create and init basic project b01

---
+++ v9/learn/fun/mvc/LearnNet9MVCFunLes01/LearnNet9MVCFunLes01Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LearnNet9MVCFunLes01Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
