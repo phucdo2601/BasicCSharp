eaafd4c29842edd48550753db45517d6d489d5a2
Phuc Do Ngoc <ngocphucdo2601@gmail.com>
2025-03-29 16:08:19 +0700

    [learn-net9-mcv-b01] 02. Basic setup entity framework in this project b01

---
+++ v9/learn/fun/mvc/LearnNet9MVCFunLes01/LearnNet9MVCFunLes01Web/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using LearnNet9MVCFunLes01Web.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LearnNet9MVCFunLes01Web.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) 
+        {
+            
+        }
+
+        #region the region import and export data
+        public DbSet<Category> Categories { get; set; }
+        #endregion
+    }
+}
--- v9/learn/fun/mvc/LearnNet9MVCFunLes01/LearnNet9MVCFunLes01Web/Program.cs
+++ v9/learn/fun/mvc/LearnNet9MVCFunLes01/LearnNet9MVCFunLes01Web/Program.cs
@@ -1,7 +1,16 @@
+using LearnNet9MVCFunLes01Web.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+// Add Db connection Entity framework
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DbConnection"));
+});
 
 var app = builder.Build();
 
